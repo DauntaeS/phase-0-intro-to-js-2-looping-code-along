c55f91f38bbfef603486e9c2d5b1e08c96b40a19
Dauntae Stewart <dauntae9@gmail.com>
2022-03-21 20:05:40 -0500

    commit tree 460654105444a770de861b0f60fbf67c335a196b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards(name1, event) {
+    let newArray = [];
+    for (let i = 0; i < name1.length; i++) {
+   let message = (`Thank you, ${name1[i]}, for the wonderful ${event} gift!`);
+   newArray.push(message);
+    }
+    return newArray; 
+}   
+
+function countDown(number) {
+    let count = [];
+    while (count < 11) {
+        console.log(count++);
+    } 
+}
